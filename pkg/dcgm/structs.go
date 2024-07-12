3f1f7dca778118768b5d7ac7e13724ee7d7c0512
程东明 <chengdm@sugon.com>
2024-07-12 14:27:53 +0800

    删除idea文件夹

--- pkg/dcgm/structs.go
+++ pkg/dcgm/structs.go
@@ -1,5 +1,13 @@
 package dcgm
 
+/*
+#cgo CFLAGS: -Wall -I/opt/dtk-24.04/rocm_smi/include/rocm_smi
+#cgo LDFLAGS: -L/opt/dtk-24.04/rocm_smi/lib -lrocm_smi64 -Wl,--unresolved-symbols=ignore-in-object-files
+#include <stdint.h>
+#include <kfd_ioctl.h>
+#include <rocm_smi64Config.h>
+#include <rocm_smi.h>
+*/
 import "C"
 
 type RSMIBitField uint64
